03b2a39352cfa2b4a7b998dbdca1a7c36ca6e6e2
AhElb <ahmedelbaradey2@gmail.com>
2021-06-01 13:38:33 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -57,4 +57,70 @@ $("#owl-example").owlCarousel({
 
 
     }); 
+
+    $(".over").click(function(){
+$(this).next('.inner_links').toggleClass("show_dawn") ;
+$(this).children(".point").toggleClass("rotate");
+
+
+    });
+
+
+    $("#togglePassword").click(function(){
+
+
+
+
+
+
+// Check the checkbox state
+  if($("#password_input").hasClass("pass")){
+
+   // Changing type attribute
+$("#password_input").attr('type','text') ;
+$("#password_input").removeClass("pass");
+$("#password_input").addClass("showt");
+    $("#togglePassword").removeClass('far fa-eye');
+        $("#togglePassword").addClass('fas fa-eye-slash');
+
+
+
+
+   
+  }
+  else if ($("#password_input").hasClass("showt")) {
+
+
+
+$("#password_input").attr('type','password') ;
+$("#password_input").addClass("pass");
+
+
+$("#password_input").removeClass("showt");
+
+$("#togglePassword").removeClass('fas fa-eye-slash');
+        $("#togglePassword").addClass('far fa-eye ');
+
+
+   
+  }
+
+           
+
+    });
+
+
+        $(".trans_btn").click(function(){
+
+
+
+$(".Transactions").css({right: 0});
+$(".all").css({right: 3000+"px" });
+
+$(this).addClass("linke_acctive");
+$('.acc_btn').removeClass("linke_acctive")
+
+
+        });
+
 });
